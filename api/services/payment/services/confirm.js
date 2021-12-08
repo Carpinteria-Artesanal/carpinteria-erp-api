44bdb94f99f439b69b02b15e14ec404e2211274c
Eduardo Parra <edue17@gmail.com>
2021-12-08 17:54:20 +0100

    confirm payment

--- api/services/payment/services/confirm.js
+++ api/services/payment/services/confirm.js
@@ -1,54 +1,26 @@
 /* eslint-disable nonblock-statement-body-position */
 const {
-  PaymentModel,
   InvoiceModel,
 } = require('carpinteria-erp-models');
 
 /**
- * Actualiza las facturas con la información del pago
- * @param {Array<string>} invoices
- * @param {Object} paymentData
- * @returns {Promise<void>}
- * @private
- */
-const _updateInvoices = async ({
-  invoices,
-  nOrder,
-}, paymentData) => {
-  const payment = {
-    ...paymentData,
-    ...(invoices.length > 1 && { invoicesOrder: nOrder }),
-  };
-  for (const invoiceId of invoices)
-    await InvoiceModel.findOneAndUpdate({ _id: invoiceId }, { payment });
-};
-/**
- * Confirma la realización del pago
+ * Edit product in delivery order invoice
  * @param {String} id
- * @param {number} paymentDate
- * @param {string} type
- * @param {string} numCheque
- * @returns {Promise<void>}
+ * @param {String} invoice
  */
-const confirm = async ({
-  params: { id },
-  body: {
-    paymentDate,
-    type,
-    numCheque,
+const confirm = ({
+  invoice,
+  id,
+}) => InvoiceModel.updateOne({
+  _id: invoice,
+  'payments._id': id,
+}, {
+  $set: {
+    'products.$[j].paid': true,
   },
-}) => {
-  const paymentData = {
-    paymentDate,
-    type,
-    ...(numCheque && { numCheque }),
-    paid: true,
-  };
+}, {
+  new: true,
+});
 
-  const payment = await PaymentModel.findOne({ _id: id });
-  await _updateInvoices(payment, paymentData);
-  await PaymentModel.deleteOne({ _id: id });
-  await PaymentModel.deleteMany({ _id: { $in: payment.payments } });
-};
 
 module.exports = confirm;
