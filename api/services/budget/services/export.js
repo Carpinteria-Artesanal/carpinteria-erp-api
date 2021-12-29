8c6b8941f59f1a8b17cd35868fd2b9dad34171db
Eduardo Parra <edue17@gmail.com>
2021-12-29 23:27:56 +0100

    fix

--- api/services/budget/services/export.js
+++ api/services/budget/services/export.js
@@ -1,10 +1,10 @@
 const carbone = require('carbone');
-const { ClientInvoiceModel, ClientModel } = require('carpinteria-erp-models');
+const { BudgetModel, ClientModel } = require('carpinteria-erp-models');
 const { formatDate } = require('../../../../utils');
 
-const INVOICE_1_TEMPLATE = './plantillas/presupuesto-1-hoja.ods';
-const INVOICE_2_TEMPLATE = './plantillas/presupuesto-2-hojas.ods';
-const INVOICE_3_TEMPLATE = './plantillas/presupuesto-3-hojas.ods';
+const BUDGET_1_TEMPLATE = './plantillas/presupuesto-1-hoja.ods';
+const BUDGET_2_TEMPLATE = './plantillas/presupuesto-2-hojas.ods';
+const BUDGET_3_TEMPLATE = './plantillas/presupuesto-3-hojas.ods';
 
 /* istanbul ignore next */
 const _productAdapter = product => ({
@@ -16,41 +16,41 @@ const _productAdapter = product => ({
 });
 
 /* istanbul ignore next */
-const _invoicesAdapter = invoice => {
-  const filasAll = invoice.products.map(_productAdapter)
+const _budgetAdapter = budget => {
+  const filasAll = budget.products.map(_productAdapter)
     .flat();
 
   return ({
-    fecha: formatDate(invoice.date),
-    nFactura: invoice.nInvoice,
-    empresa: invoice.client.businessName,
-    direccion: invoice.client.address,
-    ciudad: invoice.client.city,
-    cp: invoice.client.postalCode,
-    provincia: invoice.client.province,
-    cif: invoice.client.cif,
+    fecha: formatDate(budget.date),
+    nFactura: budget.nInvoice,
+    empresa: budget.client.businessName,
+    direccion: budget.client.address,
+    ciudad: budget.client.city,
+    cp: budget.client.postalCode,
+    provincia: budget.client.province,
+    cif: budget.client.cif,
     filas: filasAll.slice(0, 48),
     filas2: filasAll.slice(49, 97),
     filas3: filasAll.slice(98, 200),
     nFilas: filasAll.length,
-    base: invoice.taxBase,
-    iva: invoice.iva,
-    ivaTexto: invoice.products?.[0]?.iva || 0,
-    total: invoice.total,
+    base: budget.taxBase,
+    iva: budget.iva,
+    ivaTexto: budget.products?.[0]?.iva || 0,
+    total: budget.total,
   });
 };
 
 /* istanbul ignore next */
 const _getInvoice = async id => {
-  const invoice = await ClientInvoiceModel.findOne({ _id: id }).populate('client', null, ClientModel);
+  const budget = await BudgetModel.findOne({ _id: id }).populate('client', null, ClientModel);
 
-  return _invoicesAdapter(invoice);
+  return _budgetAdapter(budget);
 };
 
 const _getTemplate = ({ nFilas }) => {
-  if (nFilas < 30) return INVOICE_1_TEMPLATE;
-  if (nFilas < 59) return INVOICE_2_TEMPLATE;
-  return INVOICE_3_TEMPLATE;
+  if (nFilas < 30) return BUDGET_1_TEMPLATE;
+  if (nFilas < 59) return BUDGET_2_TEMPLATE;
+  return BUDGET_3_TEMPLATE;
 };
 
 /* istanbul ignore next */
--- api/services/deliveryorder/services/export.js
+++ api/services/deliveryorder/services/export.js
@@ -1,10 +1,10 @@
 const carbone = require('carbone');
-const { ClientInvoiceModel, ClientModel } = require('carpinteria-erp-models');
+const { DeliveryOrderModel, ClientModel } = require('carpinteria-erp-models');
 const { formatDate } = require('../../../../utils');
 
-const INVOICE_1_TEMPLATE = './plantillas/albaran-1-hoja.ods';
-const INVOICE_2_TEMPLATE = './plantillas/albaran-2-hojas.ods';
-const INVOICE_3_TEMPLATE = './plantillas/albaran-3-hojas.ods';
+const DELIVERY_ORDER_1_TEMPLATE = './plantillas/albaran-1-hoja.ods';
+const DELIVERY_ORDER_2_TEMPLATE = './plantillas/albaran-2-hojas.ods';
+const DELIVERY_ORDER_3_TEMPLATE = './plantillas/albaran-3-hojas.ods';
 
 /* istanbul ignore next */
 const _productAdapter = product => ({
@@ -16,53 +16,53 @@ const _productAdapter = product => ({
 });
 
 /* istanbul ignore next */
-const _invoicesAdapter = invoice => {
-  const filasAll = invoice.products.map(_productAdapter)
+const _deliveryOrderAdapter = deliveryOrder => {
+  const filasAll = deliveryOrder.products.map(_productAdapter)
     .flat();
 
   return ({
-    fecha: formatDate(invoice.date),
-    nFactura: invoice.nInvoice,
-    empresa: invoice.client.businessName,
-    direccion: invoice.client.address,
-    ciudad: invoice.client.city,
-    cp: invoice.client.postalCode,
-    provincia: invoice.client.province,
-    cif: invoice.client.cif,
+    fecha: formatDate(deliveryOrder.date),
+    nFactura: deliveryOrder.nInvoice,
+    empresa: deliveryOrder.client.businessName,
+    direccion: deliveryOrder.client.address,
+    ciudad: deliveryOrder.client.city,
+    cp: deliveryOrder.client.postalCode,
+    provincia: deliveryOrder.client.province,
+    cif: deliveryOrder.client.cif,
     filas: filasAll.slice(0, 48),
     filas2: filasAll.slice(49, 97),
     filas3: filasAll.slice(98, 200),
     nFilas: filasAll.length,
-    base: invoice.taxBase,
-    iva: invoice.iva,
-    ivaTexto: invoice.products?.[0]?.iva || 0,
-    total: invoice.total,
+    base: deliveryOrder.taxBase,
+    iva: deliveryOrder.iva,
+    ivaTexto: deliveryOrder.products?.[0]?.iva || 0,
+    total: deliveryOrder.total,
   });
 };
 
 /* istanbul ignore next */
-const _getInvoice = async id => {
-  const invoice = await ClientInvoiceModel.findOne({ _id: id }).populate('client', null, ClientModel);
+const _getDeliveryOrder = async id => {
+  const deliveryOrder = await DeliveryOrderModel.findOne({ _id: id }).populate('client', null, ClientModel);
 
-  return _invoicesAdapter(invoice);
+  return _deliveryOrderAdapter(deliveryOrder);
 };
 
 const _getTemplate = ({ nFilas }) => {
-  if (nFilas < 30) return INVOICE_1_TEMPLATE;
-  if (nFilas < 59) return INVOICE_2_TEMPLATE;
-  return INVOICE_3_TEMPLATE;
+  if (nFilas < 30) return DELIVERY_ORDER_1_TEMPLATE;
+  if (nFilas < 59) return DELIVERY_ORDER_2_TEMPLATE;
+  return DELIVERY_ORDER_3_TEMPLATE;
 };
 
 /* istanbul ignore next */
 const exportOds = async ({ id }) => {
-  const invoice = await _getInvoice(id);
+  const deliveryOrder = await _getDeliveryOrder(id);
 
   let bookFile = null;
   let error = null;
 
-  const template = _getTemplate(invoice);
+  const template = _getTemplate(deliveryOrder);
 
-  carbone.render(template, invoice, {
+  carbone.render(template, deliveryOrder, {
     lang: 'es-es',
   }, (err, result) => {
     /* istanbul ignore next */
